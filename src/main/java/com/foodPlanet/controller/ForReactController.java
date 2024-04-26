a69dc177bce550570493aaece04e2568a30cdeee
LeeEunDeok <t01092561402@gmail.com>
2024-04-26 14:22:55 +0900

    CORS 문제 SecurityCofing로 해결

---
+++ src/main/java/com/foodPlanet/controller/ForReactController.java
@@ -0,0 +1,16 @@
+package com.foodPlanet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "react/data")
+public class ForReactController {
+    // http://localhost:8888/react/data/exam01
+    @GetMapping(value = "/exam01")
+    public String getData01() {
+
+        return "aa/bb";
+    }
+}
